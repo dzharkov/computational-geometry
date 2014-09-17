b2ae6c5a1eab98ca9fa1348db280a69215a3c67f
Denis Zharkov <denis.zharkov@jetbrains.com>
2014-09-17 18:40:28 +0400

    simplified algoritm for first task

--- src/main/kotlin/task01/main.kt
+++ src/main/kotlin/task01/main.kt
@@ -34,90 +34,56 @@ data class Vector(val x: Int, val y: Int) {
 }
 
 data class Segment(val p1: Point, val p2: Point) {
-    val minX : Int get() = Math.min(p1.x, p2.x)
-    val maxX : Int get() = Math.max(p1.x, p2.x)
-    val isParallelToOX : Boolean get() = p1.y == p2.y
+    val minX = Math.min(p1.x, p2.x)
+    val maxX = Math.max(p1.x, p2.x)
+    val isParallelToOX = p1.y == p2.y
 
-    fun includeY(y: Int) = Math.min(p1.y, p2.y) <= y && y <= Math.max(p1.y, p2.y)
+    val top = if (p1.y > p2.y) p1 else p2
+    val bottom = if (p1.y > p2.y) p2 else p1
+
+    fun includeY(y: Int) = y in bottom.y..top.y
 
     /**
-     * returns cross product of vectors: (p2 - p1), (p - p1)
+     * returns cross product of vectors: (top - bottom), (p - bottom)
      */
-    fun crossProductSign(p: Point) : Int = (p2 - p1).crossProduct(p - p1).sign()
+    fun crossProductSign(p: Point) : Int = (top - bottom).crossProduct(p - bottom).sign()
 
     fun containsPoint(p: Point) = includeY(p.y) &&
             minX <= p.x && p.x <= maxX &&
             crossProductSign(p) == 0
 }
 
-class Polygon(pointsUnshifted: Array<Point>) {
-    val points : Array<Point>
-    val mergedSegments: MutableList<Segment> = arrayListOf();
+class Polygon(val points : Array<Point>) {
+    val segments = points.indices.map { i -> Segment(points[i], points.getCyclic(i + 1)) }
 
-    {
-        val minIndex = pointsUnshifted.indices.minBy { i -> pointsUnshifted[i] }!!
-
-        points = Array(pointsUnshifted.size) {
-            i -> pointsUnshifted.getCyclic(minIndex + i)
-        }
-
-        var i = 0
-        while (i < points.lastIndex) {
-            var j = i + 1
-
-            if (points[i].y == points[i+1].y) {
-                while (j + 1 <= points.lastIndex && points[i].y == points[j + 1].y) {
-                    j++
-                }
-            }
-
-            mergedSegments.add(Segment(points[i], points[j]))
-            i = j
-        }
-
-        mergedSegments.add(Segment(points.last(), points.first()))
+    fun isPointInside(p: Point) : Boolean {
+        return segments.count {
+            if (it.containsPoint(p)) return @isPointInside true
+            isSegmentIntersectedByRay(it, p)
+        } % 2 != 0
     }
 
-    fun isPointInside(p: Point) = mergedSegments.any { s -> s.containsPoint(p) } ||
-            (mergedSegments.indices.count { isSegmentIntersected(p, it) }) % 2 != 0
-
-    fun isSegmentIntersected(p: Point, segmentIndex: Int) : Boolean {
-        val segment = mergedSegments[segmentIndex]
-        val nextSegment = mergedSegments.getCyclic(segmentIndex + 1)
-        val prevSegment = mergedSegments.getCyclic(segmentIndex - 1)
-
-        // trace horizontal beam to the left
-
+    fun isSegmentIntersectedByRay(segment: Segment, p: Point): Boolean {
         if (segment.isParallelToOX) {
-            if (segment.p1.y == p.y && segment.minX <= p.x) {
-                return belongToDifferentHorizontalSemiplanes(nextSegment, prevSegment, p.y)
-            }
             return false
         }
 
-        if (segment.p1.y == p.y) {
-            return segment.p1.x <= p.x && belongToDifferentHorizontalSemiplanes(segment, prevSegment, p.y)
+        if (segment.top.y == p.y) {
+            return segment.top.x < p.x
         }
 
-        if (segment.p2.y == p.y) {
-            // if beam intersects border point it does so for two of segments
+        if (segment.bottom.y == p.y) {
+            // if ray intersects border point it does so for two of segments
             // so we should count it only for first of them
             return false
         }
 
-        val isPointByTheRightSideOfSegment: Boolean = segment.crossProductSign(p) * (segment.p2.y - segment.p1.y) < 0
+        val isPointByTheRightSideOfSegment: Boolean = segment.crossProductSign(p) < 0
 
         return segment.includeY(p.y) && isPointByTheRightSideOfSegment
     }
 }
 
-fun belongToDifferentHorizontalSemiplanes(a: Segment, b: Segment,  y: Int) : Boolean {
-    val deltas = listOf(a.p1.y, a.p2.y, b.p1.y, b.p2.y).map { it - y }.sort()
-
-    return deltas.first!! < 0 && deltas.last!! > 0
-}
-
-fun <T> MutableList<T>.getCyclic(i: Int) = this[(i + this.size) % this.size]
 fun <T> Array<T>.getCyclic(i: Int) = this[(i + this.size) % this.size]
 fun Long.sign() : Int {
     if (this > 0L) return 1
